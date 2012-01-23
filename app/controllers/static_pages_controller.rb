ea345ba371861b471b80229df2c47b5146af95d5
NewYork00 <newyork00@gmail.com>
2012-01-23 11:58:59 -0500

    before moving to pretty RSpec in chap 5

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,7 @@
 class StaticPagesController < ApplicationController
+  def contact
+  end
+  
   def home
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,17 @@
 SampleApp::Application.routes.draw do
   #match '/home', :to => 'static_pages#home'
   #root :to => 'static_pages/home'
-  get 'static_pages/home'
-  match '/', :to => 'static_pages#home'
-  match '/home', :to => 'static_pages#home'
-  match '/about', :to => 'static_pages#about'
-  match '/help', :to => 'static_pages#help'
-  match '/contact', :to => 'static_pages#contact'
+#  get 'static_pages/home'
+#  get 'static_pages/contact'
+#  get 'static_pages/about'
+#  get 'static_pages/help'
+#  match '/', :to => 'static_pages#home'
+#  match '/home', :to => 'static_pages#home'
+  match '/about', to: 'static_pages#about'
+  match '/help', to: 'static_pages#help'
+  match '/contact', to: 'static_pages#contact'
+
+  root to: 'static_pages#home'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
