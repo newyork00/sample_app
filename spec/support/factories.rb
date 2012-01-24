762cc2f83ca6a060d216fda33edc43ed2ec70064
NewYork00 <newyork00@gmail.com>
2012-01-24 01:23:53 -0500

    added user factory and tests

---
+++ spec/support/factories.rb
@@ -0,0 +1,7 @@
+Factory.define :user do |user|
+  user.name                   "Charles Shen"
+  user.email                  "abc@de.com"
+  user.password               "foobar"
+  user.password_confirmation  "foobar"
+end
+
